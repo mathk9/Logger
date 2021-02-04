2dccac6095ef73ef7723369f8de330d5fab056f2
Matheus de Novais Souza <math.novais@outlook.com>
2021-02-03 23:31:27 -0300

    Logger Auditoria

---
+++ Logger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterLog.Log(String.Format($"{"Log criado em "} : {DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss:fff")}"), "ArquivoLog");
+            RegisterLog.Log("Registro de log de auditoria");
+
+            Console.WriteLine("Log criado e registrado com sucesso");
+            Console.ReadLine();
+        }
+    }
+}
